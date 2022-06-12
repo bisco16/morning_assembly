2189899d5c94018d0f0f1062202e8eff4f42130e
bisco16 <104501915+bisco16@users.noreply.github.com>
2022-06-12 15:37:00 +0900

    Update recap.js

--- js/recap.js
+++ js/recap.js
@@ -1,22 +1,24 @@
 "use strict";
-    function checkRecaptcha() {
+
+function checkRecaptcha() {
     var response = grecaptcha.getResponse();
-    if(response.length == 0) {
-    //reCaptcha 未認証
+    if (response.length == 0) {
+        //reCaptcha 未認証
         window.alert("reCaptchaが未認証です");
         return false;
+    } else {
+        //reCaptch 認証
+        document.myForm.elements['entry.1099493155'].value = response; // recaptchaのname属性
+        document.myForm.submit();
     }
-    else { 
-//reCaptch 認証
-    document.myForm.elements['entry.1099493155'].value = response; // recaptchaのname属性
-    document.myForm.submit();
-    }
-    }
+}
 
-    function showThxMessage(){
-    var text = document.myForm.elements['entry.896218278']; // メールアドレスのname属性
-    if(text !== ''){
-    document.myForm.reset();
-    window.alert("お問い合わせありがとうございました。");
+function showThxMessage() {
+    var text = document.myForm.elements['entry.896218278'].value; // ニックネームのname属性
+    if (text !== '') {
+        document.myForm.reset();
+        document.getElementById('formWrapper').style.display = 'none';
+        document.getElementById('thxMessage').style.display = 'block';
+        window.alert("ご協力ありがとうございました")
     }
 }
