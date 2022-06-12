7b98fc9699b9f667f233392184cd836a551ef449
bisco16 <104501915+bisco16@users.noreply.github.com>
2022-06-12 15:00:09 +0900

    Update recap.js

--- js/recap.js
+++ js/recap.js
@@ -14,7 +14,7 @@
     }
 
     function showThxMessage(){
-    var text = document.myForm.elements['entry.896218278']; // メールアドレスのname属性
+    var text = document.myForm.elements['entry.896218278'].value; // メールアドレスのname属性
     if(text !== ''){
     document.myForm.reset();
 //     document.getElementById('thxMessage').style.display = 'block';
